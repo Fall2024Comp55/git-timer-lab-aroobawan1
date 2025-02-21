3031c9e7821fb4c0494d89d5963d1248b670c06c
aroob awan <aroobawan64@gmail.com>
2025-02-20 17:54:36 -0800

    comment
    First thing that timer needs is the timer object with 2 parameters of
    how many times i want my timer to run and 2nd responsible variable.
    2nd is we need actionlisteners like we used actionperformed.
    third we need to start the timer by using start.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,12 +1,14 @@
 import acm.graphics.*;
 import acm.program.*;
-
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.*; 
+import java.awt.event.*; 
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
-
+  private int numTimes =0; 
+  private Timer timer = new Timer(1000, this);
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
@@ -15,8 +17,19 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		timer.setInitialDelay(3);
+		timer.start();
+		
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+     if(numTimes == 10) {
+    	 timer.stop(); 
+     }
+		myLabel.move(5, 0);
+	}
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
 	}
